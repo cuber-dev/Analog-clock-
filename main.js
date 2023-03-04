71106855b8e3b8dc8d966bd3f44794d1e4920541
adepunandhu9@gmail.com <adepunandhu9@gmail.com>
2023-03-04 17:46:17 +0530

    hdhdhggss

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ let update = () => {
   let setSeconds = ((time.getSeconds()) / 60 ) * 360;
   let setHours = ((time.getHours()) / 12) * 360;
   let setMinutes = ((time.getMinutes()) / 60) * 360;
-  console.log(setSeconds);
   seconds.style.transform = `rotate(${setSeconds}deg)`;
   hours.style.transform = `rotate(${setHours}deg)`;
   minutes.style.transform = `rotate(${setMinutes}deg)`;
